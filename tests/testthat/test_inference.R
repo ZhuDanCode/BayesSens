f1e77f5efb4cc3d36656afebaa8e2c055a1279e2
kcf-jackson <kcf.jackson@gmail.com>
2018-02-14 20:48:48 +1100

    Add test against frequentist approach

---
+++ tests/testthat/test_inference.R
@@ -0,0 +1,18 @@
+library(autoBayes)
+test_that("gaussian_Gibbs", {
+  skip_on_cran()
+  n <- 1000
+  p <- 5
+  set.seed(123)
+  data0 <- gaussian_data(n, p)
+  res <- gaussian_Gibbs(data0$X, data0$y,
+    b_0 = rnorm(p), B_0 = pdmatrix(p)$Sigma, alpha_0 = 13, delta_0 = 8,
+  )
+  # check with frequentist approach
+  glm_model0 <- glm(data0$y ~ data0$X - 1, family = gaussian())
+  glm_beta <- coefficients(glm_model0)         # beta
+  glm_sigma <- sqrt(deviance(glm_model0) / n)  # sigma
+  # test
+  expect_lt(sum(abs(apply(res$beta, 2, mean) - glm_beta)), 1 / sqrt(n))
+  expect_lt(abs(mean(res$sigma) - glm_sigma), 1 / sqrt(n))
+})
