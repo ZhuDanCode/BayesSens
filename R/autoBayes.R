6cd0017a3a7c827b9b6de16348de0c48b87f09c4
kcf-jackson <kcf.jackson@gmail.com>
2018-02-25 18:01:31 +1100

    Update dependencies and documentation

--- R/autoBayes.R
+++ R/autoBayes.R
@@ -1,8 +1,8 @@
 #' Import functions
 #' @name Import_functions
-#' @importFrom stats runif rnorm rgamma setNames
+#' @importFrom stats runif rnorm rgamma setNames dgamma integrate pgamma
 #' @importFrom graphics hist par
-#' @importFrom utils tail
-#' @importFrom magrittr %>%
+#' @importFrom utils tail setTxtProgressBar txtProgressBar
+#' @importFrom magrittr %>% %<>%
 utils::globalVariables(".")
 NULL
