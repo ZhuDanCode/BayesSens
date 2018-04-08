0ab5182f723bce48c45ad4948418d2f7e5cea2a3
kcf-jackson <kcf.jackson@gmail.com>
2018-04-09 05:13:00 +1000

    Reorganise files and add examples

--- R/VAR.R
+++ R/VAR.R
@@ -1,53 +1,5 @@
-#' Simulate data from a Vector AutoRegressive (VAR) model
-#' @param m integer; number of datapoints.
-#' @param n integer; dimension of the observation.
-#' @param p integer; the time-series lag.
-#' @param init_y_t numeric matrix (optional); the initial state of the time series.
-#' The matrix must have as many columns as the lag, and the first
-#' column should be the state at the first time point.
-#' @param b_0 numeric vector (optional); the intercept.
-#' @param B list of numeric matrices (optional); the regression coefficients.
-#' @param Sigma numeric matrix (optional); the covariance matrix of the error.
-#' @examples
-#' simulate_VAR(30, 3, 2)
-#' @export
-simulate_VAR <- function(m, n, p, init_y_t, b_0, B, Sigma) {
-  if (missing(b_0)) b_0 <- rnorm(n)
-  if (missing(B)) B <- purrr::map(1:p, ~stationary_matrix(n, 1/p))
-  if (missing(Sigma)) Sigma <- pdmatrix(n)$Sigma
-
-  y_t <- matrix(0, nrow = n, ncol = m)
-  if (missing(init_y_t)) {
-    y_t[,(m-p+1):m] <- matrix(rnorm(n*p), nrow = n, ncol = p)
-  } else {
-    assertthat::are_equal(ncol(init_y_t), p)
-    y_t[,(m-p+1):m] <- init_y_t
-  }
-
-  for (i in (m-p):1) {
-    y_t[,i] <- project_ts(b_0, B, y_t[, (i+1):(i+p), drop = FALSE], Sigma)
-  }
-
-  list(data = t(y_t[, rev(1:m)]), model = list(b_0 = b_0, B = B, Sigma = Sigma))
-}
-
-project_ts <- function(b, B, y_t, Sigma) {
-  z <- b
-  for (i in seq_along(B)) {
-    z <- z + B[[i]] %*% y_t[,i]
-  }
-  z + MASS::mvrnorm(mu = numeric(length(b)), Sigma = Sigma)
-}
-
-stationary_matrix <- function(k, upper_bound = 1) {
-  res <- svd(pdmatrix(k)$Sigma)
-  res$u %*% diag(upper_bound * res$d / max(res$d)) %*% t(res$v)
-}
-
-
-# Inference
 #' Model inference for Bayesian Vector-AutoRegressive (VAR) model with
-#' normal-inverse-Wishart priors.
+#' independent normal-inverse-Wishart priors.
 #' @param data0 Matrix; each row is one observation, each column is one measurement / predictor.
 #' @param lag Integer; the lag of the time series model.
 #' @param b_0 A numeric vector; the mean for the multivariate normal prior.
@@ -57,9 +9,26 @@ stationary_matrix <- function(k, upper_bound = 1) {
 #' @param init_Sigma (Optional) matrix; the starting value of the noise inverse-covariance.
 #' @param num_steps integer; number of MCMC steps.
 #' @param burn_ins integer; number of burn-ins.
+#' @return A list of two matrices. The first matrix contains the posterior samples
+#' of beta and the second matrix contains the posterior samples of Sigma. In both
+#' matrices, each row corresponding to 1 sample of the vectorised parameter.
+#' @examples
+#' sim <- simulate_VAR(m = 1000, 3, 2) # 1000 pts of 3-dimensional lag-2 time series
+#' data0 <- sim$data
+#'
+#' dim_data <- 3
+#' lag <- 2
+#' d <- dim_data + dim_data ^2 * lag  # dimension of the vectorised parameter
+#'
+#' res <- VAR_Gibbs(
+#'   data0, lag,
+#'   b_0 = rnorm(d), B_0 = pdmatrix(d)$Sigma,
+#'   v_0 = 3, S_0 = pdmatrix(dim_data)$Sigma,
+#'   init_Sigma = diag(dim_data)
+#' )
 #' @export
 VAR_Gibbs <- function(data0, lag, b_0, B_0, v_0, S_0, init_Sigma,
-                        num_steps = 3e3, burn_ins = 1e3) {
+                      num_steps = 3e3, burn_ins = 1e3) {
   data0 <- train_data(data0, lag)
   Y <- data0$Y
   X <- data0$X
--- R/VAR_util.R
+++ R/VAR_util.R
@@ -1,4 +1,46 @@
+# VAR util functions
+#' Vectorise all the coefficients appeared in the model
+#' @param model0 A VAR model; the second component of the output from 'simulate_VAR'.
+#' @export
+VAR_model_to_vec <- function(model0) {
+  matrixcalc::vec(t(cbind(model0$b_0, do.call(cbind, model0$B))))
+}
+
+
+#' Convert vector back to model coefficients
+#' @param vec0 Numeric vector; vectorised model coefficients.
+#' @param n integer; dimension of the data.
+#' @export
+VAR_vec_to_model <- function(vec0, n) {
+  m0 <- matrix(vec0, ncol = n, byrow = F)
+  s <- seq(2, nrow(m0), n)
+  list(
+    b_0 = m0[1,],
+    B = purrr::map2(s, s+n-1, ~t(m0[.x:.y, , drop = F]))
+  )
+}
+
+
+#' #' Convert the data into a format ready for modelling
+#' #' @param data0 Matrix; the number of columns is equal to the number of time points,
+#' #' the number of rows is equal to the dimension of the observation. One can get
+#' #' such data from the first component of the output from 'simulate_VAR'.
+#' #' @param p integer; time series lag.
+#' #' @export
+#' VAR_model_matrix <- function(data0, p) {
+#'   eff_T <- ncol(data0) - p
+#'   n <- nrow(data0)
+#'   I_n <- diag(n)
+#'   purrr::map2(
+#'     .x = seq(eff_T), .y = p + seq(eff_T) - 1,
+#'     .f = ~ I_n %x% t( c(1, matrixcalc::vec(data0[, .y:.x, drop = F])) )
+#'   ) %>%
+#'     do.call(rbind, .)
+#' }
+
+
 # Create training data given a time series
+#' @keywords internal
 train_data <- function(Y, lag) {
   nr <- nrow(Y)
   nc <- ncol(Y)
