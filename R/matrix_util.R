f5bd50b254581ba2d5952232ff32ed681c6cf469
kcf-jackson <kcf.jackson@gmail.com>
2018-04-19 08:27:34 +1000

    Remove commented code

--- R/matrix_util.R
+++ R/matrix_util.R
@@ -23,16 +23,6 @@ kronecker_sp_1 <- function(A, B) {
 }
 
 
-# kronecker_sp_2 <- function(B, A) {
-#   # kronecker_special_form_1: (I_n %x% B) %*% A
-#   step <- ncol(B)
-#   do.call(rbind, purrr::map(
-#     seq(1, nrow(A), step),
-#     ~B %*% A[.x:(.x + step - 1), , drop = F]
-#   ))
-# }
-
-
 kronecker_sp_2 <- function(B, A) {
   # kronecker_special_form_1: (I_n %x% B) %*% A
   nc <- ncol(B)
@@ -61,24 +51,6 @@ kronecker_sp_2 <- function(B, A) {
 #   }
 #   C
 # }
-#
-#
-# kronecker_sp_3 <- function(B, A) {
-#   # kronecker_special_form_1: (B %x% I_n) %*% A
-#   step <- nrow(A) / ncol(B)
-#   assertthat::assert_that(step %% 1 == 0)
-#   vec_by_rowsblock <- function(i) {
-#     s <- seq(1, nrow(A), step)
-#     purrr::map(
-#       1:ncol(B),
-#       ~B[i, .x] * A[s[.x]:(s[.x] + step - 1), , drop = F]
-#     ) %>%
-#       purrr::reduce(`+`)
-#   }
-#   1:nrow(B) %>%
-#     purrr::map(vec_by_rowsblock) %>%
-#     do.call(rbind, .)
-# }
 
 
 commutation_matrix <- function(r, c) {
@@ -96,4 +68,3 @@ elimination_matrix <- function(n) {
     cbind(tgt = 1:(0.5*n*(n+1)))
   Matrix::sparseMatrix(entries[,"tgt"], entries[,"src"], x = 1)
 }
-
