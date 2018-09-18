4d72906e2ee7e7aa4a3d7fcb8c8c447d570be1df
kcf-jackson <kcf.jackson@gmail.com>
2018-09-19 02:18:06 +1000

    Update vignette and documentation

--- R/matrix_util.R
+++ R/matrix_util.R
@@ -17,31 +17,3 @@ elimination_matrix <- function(n) {
     cbind(tgt = 1:(0.5*n*(n+1)))
   Matrix::sparseMatrix(entries[,"tgt"], entries[,"src"], x = 1)
 }
-
-# commutation_matrix <- function(r, c) {
-#   m0 <- matrix(0, r*c, r*c)
-#   for (i in 1:r) {
-#     for (j in 1:c) {
-#       src <- r * (j - 1) + i
-#       tgt <- c * (i - 1) + j
-#       m0[tgt, src] <- 1
-#     }
-#   }
-#   as(m0, "dgCMatrix")
-# }
-
-# elimination_matrix <- function(n) {
-#   m0 <- matrix(0, 0.5 * n * (n + 1), n^2)
-#   src <- 1
-#   tgt <- 1
-#   for (j in 1:n) {
-#     for (i in 1:n) {
-#       if (i >= j) {
-#         m0[tgt, src] <- 1
-#         tgt <- tgt + 1
-#       }
-#       src <- src + 1
-#     }
-#   }
-#   as(m0, "dgCMatrix")
-# }
